a605fb4bfc561254a6da1acb77e97fda3cc6b1cc
abhizende <abhizende@gmail.com>
2024-09-17 19:57:12 +0530

    New commit

--- src/test/java/cucumber/options/TestRunner.java
+++ src/test/java/cucumber/options/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features/PlaceValidations.feature", glue = { "stepDefinations" })
+@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinations" })
 public class TestRunner {
-//,tags = "@DeletePlaceAPI"
+//tags = "@DeletePlaceAPI"
 }
